8db1dcebd8784d9d4d424352dabb4d86a723b250
Ben Magill <benmagill@protonmail.com>
2022-07-02 20:03:39 +0100

    add version-bump script

---
+++ scripts/version-bump.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+bump_type=$1
+
+# Handle no bump type
+if [ -z "$bump_type" ] | [ -z "$NPM_USERNAME" ] | [ -z "$NPM_PASSWORD" ]
+then 
+  exit 0
+fi
+
+# increase version
+new_version=$(npm version $bump_type)
+
+# push tag to remote
+git push origin $new_version
+
+# publish to npm
+npm publish
